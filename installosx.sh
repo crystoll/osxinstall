268e7ffa3b5244cd273fe82416ebe122f3b72623
Arto Santala <arto.santala@solita.fi>
2017-01-20 17:23:18 +0200

    Updated a lot of things in script, to keep up to date

--- installosx.sh
+++ installosx.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-brew update && brew install caskroom/cask/brew-cask && brew tap caskroom/versions
-brew cask install dropbox 
-   
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask && brew tap caskroom/versions && brew update
+
+brew cask install dropbox
+brew cask install docker
 brew cask install java 
-brew cask install google-chrome 
-brew cask install firefox 
 brew cask install google-drive
 brew cask install iterm2 
 brew cask install caffeine 
@@ -15,33 +14,34 @@ brew cask install virtualbox
 brew cask install vagrant 
 brew cask install disk-inventory-x 
 brew cask install sourcetree
-    
-brew cask install sublime-text3 
-brew cask install hipchat
-brew cask install balsamiq-mockups 
+brew cask install sublime-text
+brew cask install slack
 brew cask install intellij-idea 
-brew cask install citrix-receiver 
-brew cask install dockertoolbox 
 brew cask install poedit 
 brew cask install the-unarchiver
-    
-brew install coreutils 
-brew install z 
-brew install ack 
-brew install git 
-brew install curl 
-brew install wget 
-brew install findutils
-brew install zsh 
-brew install zsh-completions
-brew install readline 
-brew install findutils postgres 
-brew install findutils maven 
-brew install findutils gradle 
-brew install mongodb 
-brew install findutils cmake 
-brew install findutils mariadb 
-brew install findutils jenv 
-brew install findutils nvm 
-brew install findutils ansible
+brew cask install caskroom/fonts/font-hack
+brew cask install obs
+brew cask install cathode
+brew install Caskroom/cask/adpassmon
 
+    
+brew install coreutils z ack git wget findutils
+brew install jenv nvm ansible brew-gem scala sbt
+brew install readline postgres maven gradle mongodb cmake mariadb 
+brew install gource ffmpeg ec2-api-tools
+
+# Optional stuff, not needed now or already installed
+#brew cask install google-chrome 
+#brew cask install firefox 
+#brew cask install hipchat
+#brew cask install citrix-receiver 
+
+#This currently fails
+#brew cask install balsamiq-mockups 
+
+#Enable showing of hidden folders
+defaults write com.apple.finder AppleShowAllFiles YES
+killall Finder
+
+#Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
