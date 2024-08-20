73803270144cb6ee7418ffbe1ad79afec0d0105b
Arto Santala <crystoll@gmail.com>
2024-08-20 12:15:16 +0300

    Updated some things for my latest install

--- installosx.sh
+++ installosx.sh
@@ -6,10 +6,16 @@ xcode-select --install
 # Get Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Next steps
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/artosantala/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Essentials
 brew install --cask iterm2 
+
+# Here would be good to swap to iterm2
+
 brew install --cask google-chrome firefox
-brew install --cask docker
 
 # Some extra cloud services
 brew install --cask google-drive 
@@ -17,23 +23,18 @@ brew install --cask dropbox
 
 # excellent code editors
 brew install --cask sublime-text visual-studio-code
-brew install --cask intellij-idea
-
-# Teh Best Font
-brew install --cask homebrew/cask-fonts/font-hack
-
-# Excellent screen recording tool
-brew install --cask obs
-
-# Collaboration
-brew install --cask microsoft-teams slack
 
 # Incredible productivity tool
 brew install --cask spotify
 
-#Enable showing of hidden folders
+# Enable showing of hidden folders
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
+
+# Containers, using colima to host
+brew install colima docker
+brew services start colima
+
 
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -42,17 +43,23 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 brew install pyenv
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 source ~/.zshrc
-pyenv install 3.9.4
-pyenv global 3.9.4
+pyenv install 3.12
+pyenv global 3.12
 
 #install node stuff
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+nvm install 18
 
 #install java stuff
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 16.0.1.hs-adpt
+sdk install java 21.0.4-tem
 
 #install terraform stuff
 brew install warrensbox/tap/tfswitch
+
+
+## Optionals
+# brew install --cask intellij-idea
+# brew install --cask obs
 
